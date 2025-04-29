9eb2af16488781016d92bb8291a0a4ccde532aa9
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-04-29 11:00:49 -0300

    feat: create login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import UserModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 class AuthController {
     async getAllUsers(req, res) {
@@ -38,6 +39,42 @@ class AuthController {
         } catch (error) {
             console.error("Erro ao cadastrar usuário:", error);
             res.status(500).json({ error: "Erro ao cadastrar usuário" });
+        }
+    }
+
+    async login (req,res) {
+        try {
+            const { email, password } = req.body;
+
+            if(!email || !password) {
+                return res.status(400).json({ error: "Os campos de email e senha são obrigatórios" });
+            } 
+
+            const userExists = await UserModel.findByEmail(email);
+            if(!userExists) {                                  
+                return res.status(401).json({ error: "Credenciais inválidas" });
+            } 
+
+            const isPasswordValid = await bcrypt.compare(password, userExists.password);
+            if(!isPasswordValid) {
+                return res.status(401).json({ error: "Credenciais inválidas" });
+            }
+
+            const token = jwt.sign(
+                {
+                    id: userExists.id, 
+                    name: userExists.name, 
+                    email: userExists.email
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "24h",
+                }
+            )
+
+            return res.status(200).json({ message: "Login realizado com sucesso", token, userExists });
+        } catch (error) {
+            console.error("Erro ao fazer login:", error);
         }
     }
 }
